9e2a420ce87394fc014e1c5724605fbd2187599b
luis Fernando <92947513+Luifertogo@users.noreply.github.com>
2022-05-26 13:47:24 -0500

    Delete components directory

--- components/Alert.js
+++
@@ -1,9 +0,0 @@
-const Alert = ({type, messages}) => {
-  return (
-    <div className={`alert alert-${type}`}>
-     <i className='fas fa-info-circle'></i> {messages}
-    </div>
-  );
-};
-
-export default Alert;
